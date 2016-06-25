eb9c3f1edf9569566c9b1f6a14c0807a16ae7d5c
Alessio Roberto <aroberto@mobiquityinc.com>
2016-06-25 17:41:08 +0200

    First Commit

---
+++ src/SurviveAmsterdamServer/SurviveAmsterdamServer/SurviveAmsterdamServer.h
@@ -0,0 +1,19 @@
+//
+//  SurviveAmsterdamServer.h
+//  SurviveAmsterdamServer
+//
+//  Created by Alessio Roberto on 25/06/16.
+//  Copyright © 2016 Alessio Roberto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SurviveAmsterdamServer.
+FOUNDATION_EXPORT double SurviveAmsterdamServerVersionNumber;
+
+//! Project version string for SurviveAmsterdamServer.
+FOUNDATION_EXPORT const unsigned char SurviveAmsterdamServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurviveAmsterdamServer/PublicHeader.h>
+
+
