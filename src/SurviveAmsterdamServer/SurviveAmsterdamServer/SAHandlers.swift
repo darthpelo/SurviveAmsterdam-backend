dd904b245746069de50c355b4d2defe665469236
Alessio Roberto <aroberto@mobiquityinc.com>
2016-06-30 15:38:04 +0200

    Final version of the query

--- src/SurviveAmsterdamServer/SurviveAmsterdamServer/SAHandlers.swift
+++ src/SurviveAmsterdamServer/SurviveAmsterdamServer/SAHandlers.swift
@@ -156,24 +156,23 @@ final class SAHandlerProducts:PageHandler {
             
             let queries = request.queryParams
             if queries.count > 0 {
-//                let _ = query.forEach{ resultSets.append(["userid":"", "name":$0.0, "place":$0.1, "time":0, "last":false]) }
                 for query in queries {
                     if query.0 == "userid" {
-                        resultSets.append(["userid":"", "name":query.0, "place":query.1, "time":0, "last":false])
+//                        resultSets.append(["userid":"", "name":query.0, "place":query.1, "time":0, "last":false])
+                        try sqlite.forEachRow("SELECT * FROM products WHERE userid = '\(query.1)'") { (stmt, i) in
+                            
+                            // We got a result row
+                            // Pull out the values and place them in the resulting values dictionary
+                            let userid = stmt.columnText(1)
+                            let name = stmt.columnText(2)
+                            let place = stmt.columnText(3)
+                            let time = stmt.columnDouble(4)
+                            
+                            resultSets.append(["userid":userid, "name":name, "place":place, "time":time, "last":false])
+                        }
+
                     }
                 }
-                
-//                try sqlite.forEachRow("SELECT * FROM products WHERE userid = '\(user)'") { (stmt, i) in
-//                    
-//                    // We got a result row
-//                    // Pull out the values and place them in the resulting values dictionary
-//                    let userid = stmt.columnText(1)
-//                    let name = stmt.columnText(2)
-//                    let place = stmt.columnText(3)
-//                    let time = stmt.columnDouble(4)
-//                    
-//                    resultSets.append(["userid":userid, "name":name, "place":place, "time":time, "last":false])
-//                }
             } else {
                 try sqlite.forEachRow("SELECT * FROM products") { (stmt, i) in
                     
